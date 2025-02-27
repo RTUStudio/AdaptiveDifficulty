3e82034840422dcdbc8b1a3ba97b0c32183250f5
IPECTER 이팩터 <80433772+IPECTER@users.noreply.github.com>
2025-02-27 19:16:53 +0900

    AdaptiveDifficulty 1.0.1 Refactor: PAPI

--- src/main/java/com/github/ipecter/rtustudio/adaptivedifficulty/dependency/PlaceholderAPI.java
+++ src/main/java/com/github/ipecter/rtustudio/adaptivedifficulty/dependency/PlaceholderAPI.java
@@ -1,21 +1,34 @@
 package com.github.ipecter.rtustudio.adaptivedifficulty.dependency;
 
 import com.github.ipecter.rtustudio.adaptivedifficulty.AdaptiveDifficulty;
+import com.github.ipecter.rtustudio.adaptivedifficulty.configuration.DifficultyConfig;
 import com.github.ipecter.rtustudio.adaptivedifficulty.manager.StatusManager;
 import kr.rtuserver.framework.bukkit.api.dependencies.RSPlaceholder;
 import org.bukkit.OfflinePlayer;
 
 public class PlaceholderAPI extends RSPlaceholder<AdaptiveDifficulty> {
 
+    private final DifficultyConfig config;
     private final StatusManager manager;
 
     public PlaceholderAPI(AdaptiveDifficulty plugin) {
         super(plugin);
+        this.config = plugin.getDifficultyConfig();
         this.manager = plugin.getStatusManager();
     }
 
     @Override
     public String request(OfflinePlayer offlinePlayer, String[] params) {
-        return manager.get(offlinePlayer.getUniqueId());
+        switch (params[0]) {
+            case "name" -> {
+                return manager.get(offlinePlayer.getUniqueId());
+            }
+            case "display" -> {
+                DifficultyConfig.Difficulty difficulty = config.get(manager.get(offlinePlayer.getUniqueId()));
+                if (difficulty == null) return "";
+                return difficulty.displayName();
+            }
+        }
+        return "ERROR";
     }
 }
